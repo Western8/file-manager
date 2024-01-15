3293c8f410502024d432f93e67f46a4f2cc2f131
Western8 <westernchrome@gmail.com>
2024-01-16 00:22:42 +0300

    feat: add func dir up

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import os from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+let dirCur = os.homedir();
 let username = '';
 
 const parseArgs = () => {
@@ -9,21 +15,39 @@ const parseArgs = () => {
   console.log(`Welcome to the File Manager, ${username}!`);
 };
 
-parseArgs();
-
 process.on('exit', () => {
   console.log(`Thank you for using File Manager, ${username}, goodbye!`);
 });
 
-process.on('SIGINT', function() {
+process.on('SIGINT', function () {
   process.exit(0);
 });
 
-const echoInput = (chunk) => {
+const dataInput = (chunk) => {
   const chunkStringified = chunk.toString().trim();
-  if (chunkStringified === '.exit') {
-    process.exit();
+  switch (chunkStringified) {
+    case 'up':
+      goToUpDir();
+      break;
+    case '.exit':
+      process.exit();
   }
 };
 
-process.stdin.on('data', echoInput);
+process.stdin.on('data', dataInput);
+
+function showDirCur() {
+  console.log(`You are currently in ${dirCur}`)
+}
+
+function goToUpDir() {
+  let dirCurArr = dirCur.split(path.sep);
+  if (dirCurArr.length > 1) {
+    dirCurArr = dirCurArr.slice(0, -1);
+    dirCur = dirCurArr.join(path.sep);
+  }
+  showDirCur();
+} 
+
+parseArgs();
+showDirCur();
