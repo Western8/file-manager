3fd6290502125ba6506ef5a114402c4f40b5b016
Western8 <westernchrome@gmail.com>
2024-01-15 23:37:43 +0300

    feat: add exit message

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
+let username = '';
+
 const parseArgs = () => {
   const argv = Array.from(process.argv).slice(2);
   const argUsername = argv.find(item => item.split('=')[0] === '--username');
   if (argUsername) {
-    console.log(`Welcome to the File Manager, ${argUsername.split('=')[1]}!`);
+    username = argUsername.split('=')[1];
   }
+  console.log(`Welcome to the File Manager, ${username}!`);
 };
 
 parseArgs();
+
+process.on('exit', () => {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+});
+
+process.on('SIGINT', function() {
+  process.exit(0);
+});
+
+const echoInput = (chunk) => {
+  const chunkStringified = chunk.toString().trim();
+  if (chunkStringified === '.exit') {
+    process.exit();
+  }
+};
+
+process.stdin.on('data', echoInput);
